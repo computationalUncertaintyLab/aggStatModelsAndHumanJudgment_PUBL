098dead51c925cdae41394617f8c81f09f262a12
tom mcandrew <mcandrew@lehigh.edu>
2021-01-05 10:14:54 -0500

    updated data and plot

--- data/JHUCasesData/pullAndProcessJHUCasesData.py
+++ data/JHUCasesData/pullAndProcessJHUCasesData.py
@@ -4,6 +4,7 @@ import sys
 
 sys.path.append('../')
 from helperFuncs.timeAndDates import EWandMW 
+from helperFuncs.timeAndDates import buildDictionaryFromEW2EndDate
 
 import numpy as np
 import pandas as pd
@@ -28,7 +29,7 @@ class JHUCasesData(object):
         vvs = [x for x in self.casesdata_w.columns if re.match("\d+[/]\d+[/]\d+",x)]
         casesdata_l = self.casesdata_w.melt( id_vars = ["FIPS"], value_vars = vvs )
         self.casesdata_l = casesdata_l.rename(columns={'variable':'date','value':'cases'})
-        
+
     def UStotal(self):
         usTotalCases = self.casesdata_l.groupby(["date"]).sum().reset_index()[["date","cases"]]
         usTotalCases = EWandMW(usTotalCases).d
@@ -43,6 +44,9 @@ class JHUCasesData(object):
     def incidentCases(self):
         self.usTotalCases["inccases"] = self.usTotalCases.diff()["cases"]
 
+    def outputRawData(self):
+        self.casesdata_l.to_csv("./JHU_newcases_data.csv",index=False)
+
 def computeYticks(ys):
     ys = ys[~np.isnan(ys)]
     miny,maxy = min(ys),max(ys)
@@ -53,6 +57,8 @@ if __name__ == "__main__":
     jhuDD = JHUCasesData()
     jhuDD.incidentCases()
 
+    jhuDD.outputRawData()
+    
     jhuNewCases = jhuDD.usTotalCases
     jhuNewCases = jhuNewCases.iloc[:-1,:]
 
@@ -62,21 +68,24 @@ if __name__ == "__main__":
     ax.scatter( jhuNewCases.MW, jhuNewCases.inccases, s=10 )
     ax.plot( jhuNewCases.MW, jhuNewCases.inccases, lw=3, alpha=0.50)
 
+    print("Number of New Cases = {:.1f} on for most recent epiweek".format(jhuNewCases.inccases.iloc[-1]))
+    
     EWs = jhuNewCases.EW
     MWs = jhuNewCases.MW
+
+    fromEW2Date = buildDictionaryFromEW2EndDate(EWs)
+    ax.set_xticks(list(MWs[::5])+[MWs.iloc[-1]])
+    ax.set_xticklabels( [ "{:s}".format(fromEW2Date[int(x)]) for x in list(EWs[::5]) + [EWs.iloc[-1]] ], rotation=45, ha="right" )
     
-    ax.set_xticks(MWs[::5])
-    ax.set_xticklabels( [int(x) for x in EWs[::5]] )
- 
     ax.tick_params(which="both",labelsize=8)
 
-    ax.set_xlabel("Epiweek", fontsize=10)
+    ax.set_xlabel("Date", fontsize=10)
     ax.set_ylabel("Num. of new cases due to COVID-19", fontsize=10)
 
     
     utc = datetime.utcnow().date()
     
-    ax.text(0.01, 0.99, "Data as recent as {:d}-{:d}-{:d}".format( utc.year, utc.month, utc.day )
+    ax.text(0.01, 0.99, "Data as recent as {:04d}-{:02d}-{:02d}".format( utc.year, utc.month, utc.day )
             ,weight="bold",fontsize=10,ha="left",va="top", color="#3d3d3d",transform=ax.transAxes)
     
     ax.set_yticks( computeYticks(jhuNewCases.inccases) ) 
@@ -87,5 +96,5 @@ if __name__ == "__main__":
     w = mm2inch(189)
     fig.set_size_inches(w,w/1.6)
     
-    plt.savefig("numberOfNewCases.png")
+    plt.savefig("numberOfNewCases.png",dpi=300)
     plt.close()
