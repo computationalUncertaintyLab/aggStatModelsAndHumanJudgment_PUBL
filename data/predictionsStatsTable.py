cdcf5557933b607b5e6a06655cc56fd979b6109b
tom mcandrew <mcandrew@lehigh.edu>
2021-01-27 09:43:00 -0500

    update to quantiles

--- data/predictionsStatsTable.py
+++ data/predictionsStatsTable.py
@@ -10,7 +10,10 @@ from mods.datahelp import grabData
 def computeMedianAndIQR(d):
 
     stats = {"quantile":[], "value":[]}
-    for quantile in [0.025,0.05,0.10,0.25,0.50,0.75,0.90,0.95,0.975]:
+    for quantile in [ 0.010, 0.025, 0.050, 0.100, 0.150, 0.200
+                      ,0.250, 0.300, 0.350, 0.400, 0.450, 0.500, 0.550, 0.600
+                      , 0.650 ,0.700 ,0.750 ,0.800 ,0.850 ,0.900 ,0.950
+                      ,0.975, 0.990 ]:
         d["distanceFromQ"] = abs(d.cprobs - quantile)
         val = d.sort_values("distanceFromQ").iloc[0]["bin"]
 
