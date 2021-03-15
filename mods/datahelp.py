87d38de3d37c58ea5ab5f944f1d5b17d2be61dee
Fred Trotter <83133+ftrotter@users.noreply.github.com>
2021-03-15 03:25:08 -0500

    use legacy API url until port to new API.

--- mods/datahelp.py
+++ mods/datahelp.py
@@ -68,7 +68,9 @@ class grabDHSdata(object):
     # - data pull
     def grabMetaData(self):
         import requests
-        metadata = requests.get("https://healthdata.gov/api/3/action/package_show?id=83b4a668-9321-4d8c-bc4f-2bef66c49050&page=0")
+        # TODO this url will be retired entirely soon.. need to migrate the new API format.
+        # Until then, the legacy url will continue to be supported for a few weeks. 
+        metadata = requests.get("https://legacy.healthdata.gov/api/3/action/package_show?id=83b4a668-9321-4d8c-bc4f-2bef66c49050&page=0")
         metaDataDict = metadata.json()
         if metaDataDict['success']:
             metaDataDict = metaDataDict['result'][0]
